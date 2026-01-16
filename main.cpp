f3d634da7de2eb08d1ac0099eb1f7885aaf3325d
ljw <ljw2017@sjtu.edu.cn>
2026-01-16 16:14:39 +0800

    Rename div_strings to delimiters. Remove space special case for delimiters in main.cpp.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ struct Command_content
     std::string R1;
     std::string R2;
     std::string source;
-    std::string div_string;
+    std::string delimiters;
     size_t sta=0;
     size_t end=std::string::npos;
     Command_content(int argc, char **argv)
@@ -18,27 +18,25 @@ struct Command_content
                 R2=std::string(argv[i+1]);
             if(!strcmp(argv[i],"-source"))
                 source=std::string(argv[i+1]);
-            if(!strcmp(argv[i],"-div_strings"))
-                div_string=std::string(argv[i+1]);
+            if(!strcmp(argv[i],"-delimiters"))
+                delimiters=std::string(argv[i+1]);
             if(!strcmp(argv[i],"-sta"))
                 sta=stoi(std::string(argv[i+1]));
             if(!strcmp(argv[i],"-end"))
                 end=stoi(std::string(argv[i+1]));
         }
-        if(div_string=="space")
-            div_string=" ";
     }
 };
 
-std::string cut_addr(std::string &addr, std::string &div_string) {
-    int pos=addr.find_first_of(div_string);
+std::string cut_addr(std::string &addr, std::string &delimiters) {
+    int pos=addr.find_first_of(delimiters);
     return addr.substr(0, pos);
 }
 
 int main(int argc, char **argv) {
     for(int i=1; i<argc; ++i) {
         if(!strcmp(argv[i],"-h") || !strcmp(argv[i],"--help") || !strcmp(argv[i],"-help")) {
-            std::cout << "./call_r2_merge -R1 R1.fq -R2 R2.fq -source 下机.R2.fq -sta 截取起始 -end 截取终止\n";
+            std::cout << "./call_r2_merge -R1 R1.fq -R2 R2.fq -source 下机.R2.fq -delimiters \"分割字母集合\" -sta 截取起始 -end 截取终止\n";
             return 0;
         }
     }
@@ -52,7 +50,7 @@ int main(int argc, char **argv) {
     getline(fin2, addr2);
     while(fin1.good()) {
         while(fin2.good()) {
-            if(cut_addr(addr2, cc.div_string) == cut_addr(addr1, cc.div_string)) {
+            if(cut_addr(addr2, cc.delimiters) == cut_addr(addr1, cc.delimiters)) {
                 for(int i=0; i<4; ++i)
                 {
                     fout << addr2 << '\n';
